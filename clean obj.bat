f0a147a9a21d8f53a688652517cf075a4b2980ba
雪リリ <yukinshell@qq.com>
2024-12-16 11:59:38 +0800

    Create clean obj.bat

---
+++ clean obj.bat
@@ -0,0 +1,5 @@
+rd /s /q ReimaginedScheduling.Shared\obj
+rd /s /q ReimaginedScheduling.CLI.Auto\obj
+rd /s /q ReimaginedScheduling.CLI.Modifier\obj
+rd /s /q ReimaginedScheduling.CLI.Viewer\obj
+pause
