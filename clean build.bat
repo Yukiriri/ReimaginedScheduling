812d680c84d9b899a704dd389ea72557aa5c2c2a
雪リリ <yukinshell@qq.com>
2024-12-18 06:13:51 +0800

    Create clean build.bat

---
+++ clean build.bat
@@ -0,0 +1,8 @@
+rd /s /q bin
+rd /s /q ReimaginedScheduling.Shared\bin
+rd /s /q ReimaginedScheduling.Shared\obj
+rd /s /q ReimaginedScheduling.CLI.Auto\bin
+rd /s /q ReimaginedScheduling.CLI.Auto\obj
+rd /s /q ReimaginedScheduling.CLI.Modifier\obj
+rd /s /q ReimaginedScheduling.CLI.Viewer\obj
+pause
