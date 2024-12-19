c65036287fe5a5374bd3abb018d5686e4849a03d
雪リリ <yukinshell@qq.com>
2024-12-19 19:37:31 +0800

    Update clean build.bat

--- clean build.bat
+++ clean build.bat
@@ -3,6 +3,8 @@ rd /s /q ReimaginedScheduling.Shared\bin
 rd /s /q ReimaginedScheduling.Shared\obj
 rd /s /q ReimaginedScheduling.CLI.Auto\bin
 rd /s /q ReimaginedScheduling.CLI.Auto\obj
+rd /s /q ReimaginedScheduling.CLI.Manual\obj
 rd /s /q ReimaginedScheduling.CLI.Modifier\obj
+rd /s /q ReimaginedScheduling.CLI.Start\obj
 rd /s /q ReimaginedScheduling.CLI.Viewer\obj
 pause
