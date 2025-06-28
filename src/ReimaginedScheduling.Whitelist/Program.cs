5bcc0168509c2cb908ea27d26a81f3c4ee94082e
Yukiriri <50292962+Yukiriri@users.noreply.github.com>
2025-06-29 05:37:41 +0800

    改进性能

--- src/ReimaginedScheduling.Whitelist/Program.cs
+++ src/ReimaginedScheduling.Whitelist/Program.cs
@@ -6,6 +6,7 @@ using ReimaginedScheduling.Lib.Windows.Info;
 ProcessRequire.setLastCpu();
 ProcessRequire.enableSeDebug();
 
+long currentMs() => DateTimeOffset.Now.ToUnixTimeMilliseconds();
 var rotation_interval = 1000 / CpuSetInfo.PCores.Count;
 var exe_list = new List<string>();
 
@@ -17,21 +18,24 @@ for (;;Thread.Sleep(1))
         exe_list = [..MyHotSaved.reload().Split('\n').Select(x => x.TrimEnd('\r', '\n'))];
     }
 
-    var process_list = new List<Process>();
-    process_list.AddRange(Process.GetProcesses().Where(x => exe_list.Contains(x.ProcessName + ".exe")));
-    process_list.ForEach(x => Console.WriteLine(x.ProcessName + ".exe"));
-    
-    var tid_list = new List<int>();
-    tid_list.AddRange(process_list.SelectMany(process => ProcessInfo.listThreadIds(process.Id)));
-    Console.WriteLine($"all thread count: {tid_list.Count}");
+    var ms = currentMs();
+    var matched_processes = new List<Process>(Process.GetProcesses()
+        .Where(x => exe_list.Contains($"{x.ProcessName}.exe")));
+    matched_processes.ForEach(x => Console.WriteLine($"* {x.ProcessName}.exe"));
+    Console.WriteLine();
+
+    var t_cpu_infos = new List<ThreadCpuInfo>(matched_processes
+        .SelectMany(process => ProcessInfo.listThreadIds(process.Id))
+        .Select(x => new ThreadCpuInfo(x)));
+    Console.WriteLine($"all thread count: {t_cpu_infos.Count,-4} load: {currentMs() - ms}ms   ");
 
     for (var core_index = 0; core_index < CpuSetInfo.PCores.Count; core_index++)
     {
-        foreach (var tid in tid_list)
+        t_cpu_infos.ForEach(x =>
         {
-            new ThreadCpuInfo(tid).currentCpuIdealNumber = core_index;
-        }
-        Console.Write($"core index: {core_index}   \r");
+            x.currentCpuIdealNumber = core_index;
+        });
+        Console.Write($"core index: {core_index,-3}\r");
         Thread.Sleep(rotation_interval);
     }
 }
